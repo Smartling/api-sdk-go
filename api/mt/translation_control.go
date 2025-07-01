493ea24d5305f11b257b2247f96f95f5959c9278
az-smartling <anatoliy.zavodovskyy@sl-KXTLW9X40H.local>
2025-07-01 19:53:22 +0200

    updated TranslationControl

--- api/mt/translation_control.go
+++ api/mt/translation_control.go
@@ -1,10 +1,12 @@
 package mt
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/Smartling/api-sdk-go/helpers/sm_client"
-	"github.com/Smartling/api-sdk-go/helpers/sm_file"
 )
 
 // TranslationControl defines translation control behaviour
@@ -37,30 +39,102 @@ func (h httpTranslationControl) CancelTranslation(accountUID AccountUID, fileUID
 
 // DetectFileLanguage detects file language
 func (h httpTranslationControl) DetectFileLanguage(accountUID AccountUID, fileUID FileUID) (DetectFileLanguageResponse, error) {
-	var res DetectFileLanguageResponse
 	startPath := buildDetectFileLanguagePath(accountUID, fileUID)
 	path := joinPath(mtBasePath, startPath)
-	_, _, err := h.base.client.PostJSON(path, nil, &res, contentTypeApplicationJson)
+
+	url := h.base.client.BaseURL + path
+	request, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return DetectFileLanguageResponse{}, fmt.Errorf("failed to create request: %v", err)
+	}
+
+	request.Header.Set("Authorization", "Bearer "+h.base.client.Credentials.AccessToken.Value)
+
+	response, err := h.base.client.HTTP.Do(request)
 	if err != nil {
 		return DetectFileLanguageResponse{}, fmt.Errorf("failed to detect file language: %w", err)
 	}
-	return res, nil
+	if response.StatusCode != http.StatusAccepted {
+		return DetectFileLanguageResponse{}, fmt.Errorf("expected 202 status got: %d", response.StatusCode)
+	}
+	defer response.Body.Close()
+
+	body, _ := io.ReadAll(response.Body)
+
+	type detectFileLanguageResponse struct {
+		Response struct {
+			Code string `json:"code"`
+			Data struct {
+				LanguageDetectionUID string `json:"languageDetectionUid"`
+			} `json:"data"`
+		} `json:"response"`
+	}
+	var res detectFileLanguageResponse
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return DetectFileLanguageResponse{}, fmt.Errorf("failed to unmarshal: %v", err)
+	}
+	return DetectFileLanguageResponse{
+		Code:                 res.Response.Code,
+		LanguageDetectionUID: res.Response.Data.LanguageDetectionUID,
+	}, nil
 }
 
 // DetectionProgress returns info about detection
 func (h httpTranslationControl) DetectionProgress(accountUID AccountUID, fileUID FileUID, languageDetectionUID string) (DetectionProgressResponse, error) {
-	var res DetectionProgressResponse
 	progressPath := buildDetectionProgressPath(accountUID, fileUID, languageDetectionUID)
 	path := joinPath(mtBasePath, progressPath)
-	_, _, err := h.base.client.GetJSON(
-		path,
-		smfile.FileURIRequest{FileURI: string(fileUID)}.GetQuery(),
-		&res,
-	)
+
+	url := h.base.client.BaseURL + path
+	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return DetectionProgressResponse{}, fmt.Errorf("failed to get detection process: %w", err)
+		return DetectionProgressResponse{}, fmt.Errorf("failed to create request: %v", err)
 	}
-	return res, nil
+	request.Header.Set("Authorization", "Bearer "+h.base.client.Credentials.AccessToken.Value)
+
+	response, err := h.base.client.HTTP.Do(request)
+	if err != nil {
+		return DetectionProgressResponse{}, fmt.Errorf("failed to detect file language: %w", err)
+	}
+	defer response.Body.Close()
+
+	body, _ := io.ReadAll(response.Body)
+
+	type detectionProgressResponse struct {
+		Response struct {
+			Code string `json:"code"`
+			Data struct {
+				State                   string  `json:"state"`
+				Error                   *string `json:"error"`
+				DetectedSourceLanguages []struct {
+					LanguageID      string `json:"languageId"`
+					DefaultLocaleID string `json:"defaultLocaleId"`
+				} `json:"detectedSourceLanguages"`
+			} `json:"data"`
+		} `json:"response"`
+	}
+	var res detectionProgressResponse
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return DetectionProgressResponse{}, fmt.Errorf("failed to unmarshal: %v", err)
+	}
+	detectedSourceLanguages := make([]DetectedSourceLanguage, len(res.Response.Data.DetectedSourceLanguages))
+	for i, v := range res.Response.Data.DetectedSourceLanguages {
+		detectedSourceLanguages[i] = DetectedSourceLanguage{
+			LanguageID:      v.LanguageID,
+			DefaultLocaleID: v.DefaultLocaleID,
+		}
+	}
+	var restErr string
+	if res.Response.Data.Error != nil {
+		restErr = *(res.Response.Data.Error)
+	}
+	return DetectionProgressResponse{
+		Code:                    res.Response.Code,
+		State:                   res.Response.Data.State,
+		Error:                   restErr,
+		DetectedSourceLanguages: detectedSourceLanguages,
+	}, nil
 }
 
 func buildCancelTranslationPath(accountUID AccountUID, fileUID FileUID, mtUID MtUID) string {
@@ -72,5 +146,5 @@ func buildDetectFileLanguagePath(accountUID AccountUID, fileUID FileUID) string 
 }
 
 func buildDetectionProgressPath(accountUID AccountUID, fileUID FileUID, languageDetectionUID string) string {
-	return "/accounts/" + string(accountUID) + "/files/" + string(fileUID) + "/mt/" + languageDetectionUID + "/status"
+	return "/accounts/" + string(accountUID) + "/files/" + string(fileUID) + "/language-detection/" + languageDetectionUID + "/status"
 }
