c80b15eb932d8958e1e554f6be947152bd61a81a
az-smartling <azavodovskyy@smartling.com>
2025-11-14 22:16:11 +0100

    validate params

--- api/mt/uid.go
+++ api/mt/uid.go
@@ -1,5 +1,30 @@
 package mt
 
+import "strings"
+
 type AccountUID string
+
+func (a AccountUID) Validate() error {
+	if strings.TrimSpace(string(a)) == "" {
+		return ErrEmptyParam("AccountUID")
+	}
+	return nil
+}
+
 type FileUID string
+
+func (f FileUID) Validate() error {
+	if strings.TrimSpace(string(f)) == "" {
+		return ErrEmptyParam("FileUID")
+	}
+	return nil
+}
+
 type MtUID string
+
+func (m MtUID) Validate() error {
+	if strings.TrimSpace(string(m)) == "" {
+		return ErrEmptyParam("MtUID")
+	}
+	return nil
+}
