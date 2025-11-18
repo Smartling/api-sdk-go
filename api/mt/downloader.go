f1faf25c974f71aefc7a5a006685e68154c5b3a0
az-smartling <azavodovskyy@smartling.com>
2025-11-18 18:33:56 +0100

    remove GET /files/zip endpoint

--- api/mt/downloader.go
+++ api/mt/downloader.go
@@ -12,7 +12,6 @@ import (
 type Downloader interface {
 	File(accountUID AccountUID, fileUID FileUID,
 		mtUID MtUID, localeID string) (io.Reader, error)
-	Batch(accountUID AccountUID, fileUID FileUID, mtUID MtUID) (io.Reader, error)
 }
 
 // NewDownloader returns new Downloader implementation
@@ -44,25 +43,6 @@ func (d httpDownloader) File(accountUID AccountUID, fileUID FileUID,
 	return reader, nil
 }
 
-// Batch download files
-func (d httpDownloader) Batch(accountUID AccountUID, fileUID FileUID, mtUID MtUID) (io.Reader, error) {
-	batchPath := buildBatchPath(accountUID, fileUID, mtUID)
-	path := joinPath(mtBasePath, batchPath)
-	reader, _, err := d.base.client.Get(
-		path,
-		smfile.FileURIRequest{FileURI: string(fileUID)}.GetQuery(),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("failed to download original file: %w", err)
-	}
-
-	return reader, nil
-}
-
 func buildFilePath(accountUID AccountUID, fileUID FileUID, mtUID MtUID, localeID string) string {
 	return "/accounts/" + string(accountUID) + "/files/" + string(fileUID) + "/mt/" + string(mtUID) + "/locales/" + localeID + "/file"
-}
-
-func buildBatchPath(accountUID AccountUID, fileUID FileUID, mtUID MtUID) string {
-	return "/accounts/" + string(accountUID) + "/files/" + string(fileUID) + "/mt/" + string(mtUID) + "/locales/all/file/zip"
 }
