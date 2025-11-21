d8a12b5ba91fd371a67ff2e1d60ad0c0fb3af293
Anatoliy Zavodovskyy <azavodovskyy@smartling.com>
2025-11-21 14:57:45 +0100

    CON-1846 validate params (#21)
    * validate params

---
+++ api/mt/err_empty_param.go
@@ -0,0 +1,31 @@
+package mt
+
+import (
+	"errors"
+	"fmt"
+)
+
+// ErrEmptyParam creates a new empty param error
+func ErrEmptyParam(name string) error {
+	return &errEmptyParam{
+		paramName: name,
+	}
+}
+
+// IsErrEmptyParam checks if error is errEmptyParam
+func IsErrEmptyParam(err error) bool {
+	if err == nil {
+		return false
+	}
+	var target *errEmptyParam
+	return errors.As(err, &target)
+}
+
+type errEmptyParam struct {
+	paramName string
+}
+
+// Error returns the string representation of the error
+func (e errEmptyParam) Error() string {
+	return fmt.Sprintf("parameter `%s` cannot be empty", e.paramName)
+}
